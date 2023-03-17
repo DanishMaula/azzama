753fb5d206656620161d6d188c47e38415195f51
Rifky Syaripudin <rks@jiwalu.id>
2023-03-17 13:41:26 +0700

    commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,20 +6,11 @@
 
 @endsection
 
+@section('swipper')
+<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+@endsection
+
 @section('content')
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
-    <title>Document</title>
-</head>
-
-<body>
     <section class="home hero">
         <div class="text-hero">
             <p class="name">AZZAMA</p>
@@ -46,17 +37,40 @@
             <a href="#" class="discover">Discover More</a>
         </div>
 
-        <div class="swipper-article">
+        <div class="swiper mySwiper">
+            {{-- <div class="swiper-container">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <img src="images/main1.jpg" alt="">
+                        <div class="caption">
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia facere incidunt quasi? Eveniet quo magnam excepturi, ea maxime dolore nemo!</p>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="images/main1.jpg" alt="">
+                        <div class="caption"></div>
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="images/main1.jpg" alt="">
+                        <div class="caption">
 
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="images/main1.jpg" alt="">
+                        <div class="caption"></div>
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="images/main1.jpg" alt="">
+                        <div class="caption"></div>
+                    </div>
+                    <div class="swiper-slide">
+                        <img src="images/main1.jpg" alt="">
+                        <div class="caption"></div>
+                    </div>
+                </div>
+            </div> --}}
         </div>
     </section>
 
-
-    <!--- JS --->
-    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
-
-    <script src="{{ asset('js/script.js') }}"></script>
-</body>
-
-</html>
 @endsection
